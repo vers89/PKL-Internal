13881d2702f6ea858f63619ac75b822f4f950272
vers89 <virgiseptian77@gmail.com>
2022-07-07 14:03:28 +0700

    memperbaiki syntax code

--- PKL-DAY-3/jurusan/index.php
+++ PKL-DAY-3/jurusan/index.php
@@ -54,7 +54,7 @@
                 <h3>Data Guru</h3>
                 <center><a href="create.php" style="border-radius: 40px;" class="btn btn-outline-primary mt-3 ms-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-            </svg> Tambah Data</a></center>
+            </svg> Tambah Data Guru</a></center>
             </div>
             <div class="card-body">
                 <!-- TABLE DOSEN -->
